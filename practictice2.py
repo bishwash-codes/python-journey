bbebc1bc4eb983fe9b309daaa94713029fbc5ff6
bishwash-codes <bishwashpoudel000@gmail.com>
2026-08-06 13:39:37 +0545

    practiseQ

---
+++ practictice2.py
@@ -0,0 +1,23 @@
+# WAP to ask fav movies from the user and store it in a list \
+m = []
+mov=input("enter first movie ; ")
+m.append(mov)
+mov=input("enter second movie ; ")
+m.append(mov)
+mov=input("enter third movie ; ")
+m.append(mov)
+
+print(m)
+
+#simpler and efficient but with colours
+
+colors=[]
+colors.append(input('enter 1st color ; '))
+colors.append(input('enter second color ; '))
+colors.append(input('enter 3rd color ; '))
+
+print(colors)
+
+
+
+
