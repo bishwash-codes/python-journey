dcf701f3b203496559f03689b30c189f6039dfc4
bishwash-codes <bishwashpoudel000@gmail.com>
2026-08-28 14:05:48 +0545

    aug28 progressing

---
+++ whileloop.py
@@ -0,0 +1,46 @@
+# 1 to 100 using loop
+i = 1
+while i <= 100: 
+    print(i)
+    i += 1
+
+print ('end of loop')
+
+
+# 100 to 1 using loop
+j = 100 
+while j >= 1:
+    print(j)
+    j -= 1
+
+print("end of loop 2")
+
+
+# multiplication table using loop 
+
+n = int(input('enter a number'))
+m = 1 
+while m <= 10:
+    print(n*m)
+    m += 1
+print('end of table')
+
+
+# print elements of list using loop 
+# [1,4,9,16,25,36,49,64,81,100]
+
+num = [1,4,9,16,25,36,49,64,81,100]
+a=0
+while a<len(num):
+    print(num[a])
+    a += 1
+print('thank you')
+
+
+
+
+
+
+
+
+
