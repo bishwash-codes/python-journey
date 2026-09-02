a448b258f81fe552f355da5c2e083b4fdfd0e4a5
bishwash-codes <bishwashpoudel000@gmail.com>
2026-09-02 14:09:43 +0545

    interesting

---
+++ forloop.py
@@ -0,0 +1,15 @@
+# to find the value in a gvn tuple
+
+
+num = (1, 4, 9, 16, 25, 36, 49, 64, 81, 100)
+
+x = int(input("enter a no. to search "))
+i = 0
+for val in num:
+    if val == x:
+        print(val, "found at index", i)
+        break
+    i += 1
+else:
+    (print("not found"))
+print("end")
